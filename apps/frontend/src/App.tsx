84baa05f858b70a7f8642135b913364122f10aa4
leonelhv <leonel.huachacav@gmail.com>
2023-07-21 13:05:18 -0500

    wip(frontend): work in design

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from './assets/logo.svg';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    return (
+        <div className='bg-black min-h-screen'>
+            <div className='container mx-auto'>
+                <div className='flex justify-between'>
+                    <div className='border w-3/12 p-4'>
+                        <img src={logo} className='w-10 h-10' alt="" />
+                    </div>
+                    <div className='border w-1/2 p-4'>
+                        <h2 className='text-white text-2xl font-bold'>Home</h2>
+                        <div className='mt-12 flex w-full'>
+                            <div className=''>
+                                <img src="https://i.pravatar.cc/80" className='rounded-full w-[66px]' alt="" />
+                            </div>
+                            <div className='flex flex-col gap-3 w-[calc(100%-66px)] ml-4'>
+                                <span contentEditable="true" className='text-white block border-b outline-none pb-1'></span>
+                                <button className='bg-violet-900 text-white px-3 py-2 rounded-xl self-end'>Tweet</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='border w-3/12 p-4'>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App
