537d5162222d4cceab8bd0862476b192fe88d4e8
leonelhv <leonel.huachacav@gmail.com>
2023-08-01 20:48:54 -0500

    fix: secure cookies

--- apps/api/src/controllers/auth.controller.js
+++ apps/api/src/controllers/auth.controller.js
@@ -8,99 +8,97 @@ const randomRange = require("../utils/randomRange.js");
 
 const register = async (req, res) => {
 
-  const { username, email, password, name, lastname, phone } = req.body
+    const { username, email, password, name, lastname, phone } = req.body
 
-  const errors = {};
+    const errors = {};
 
-  const userFound = await User.findOne({
-    $or: [{ username }, { email }]
-  })
+    const userFound = await User.findOne({
+        $or: [{ username }, { email }]
+    })
 
-  if (userFound) {
-    if (userFound.username === username) {
-      errors["username"] = "username already used"
+    if (userFound) {
+        if (userFound.username === username) {
+            errors["username"] = "username already used"
+        }
+        if (userFound.email === email) {
+            errors["email"] = "email already used"
+        }
     }
-    if (userFound.email === email) {
-      errors["email"] = "email already used"
+
+    if (Object.keys(errors).length > 0) {
+        return responseCustom(res, 400, errors);
     }
-  }
 
-  if (Object.keys(errors).length > 0) {
-    return responseCustom(res, 400, errors);
-  }
+    const passwordHash = await bcrypt.hash(password, 10)
 
-  const passwordHash = await bcrypt.hash(password, 10)
+    const randomNumber = await randomRange(1, 3)
 
-  const randomNumber = await randomRange(1, 3)
+    const photoUser = `assets/${randomNumber}.png`
 
-  const photoUser = `assets/${randomNumber}.png`
+    const newUser = new User({
+        username,
+        email,
+        password: passwordHash,
+        name,
+        lastname,
+        phone,
+        photo: photoUser
+    })
 
-  const newUser = new User({
-    username,
-    email,
-    password: passwordHash,
-    name,
-    lastname,
-    phone,
-    photo: photoUser
-  })
+    const userSaved = await newUser.save();
 
-  const userSaved = await newUser.save();
+    const token = await createAccessToken({
+        id: userSaved._id,
+    });
 
-  const token = await createAccessToken({
-    id: userSaved._id,
-  });
+    res.cookie("token", token, {
+        httpOnly: process.env.NODE_ENV !== "development",
+        secure: false,
+        sameSite: "none",
 
-  res.cookie("token", token, {
-    // httpOnly: process.env.NODE_ENV !== "development",
-    httpOnly: false,
-    secure: false,
-    // sameSite: "none",
+    });
 
-  });
+    return responseCustom(res, 200, {
+        id: userSaved._id,
+        name: userSaved.name,
+        username: userSaved.username,
+        email: userSaved.email,
+        photo: userSaved.photo
 
-  return responseCustom(res, 200, {
-    id: userSaved._id,
-    name: userSaved.name,
-    username: userSaved.username,
-    email: userSaved.email,
-    photo: userSaved.photo
-
-  });
+    });
 }
 
 const login = async (req, res) => {
 
-  const { email, password } = req.body
+    const { email, password } = req.body
 
-  const userFound = await User.findOne({ email })
-  const isMatch = await bcrypt.compare(password, userFound.password)
+    const userFound = await User.findOne({ email })
+    const isMatch = await bcrypt.compare(password, userFound.password)
 
-  if (!userFound || !isMatch) throw new ErrorCustom("Invalid Credentials", 401)
-
-
-  const token = await createAccessToken({ id: userFound._id })
+    if (!userFound || !isMatch) throw new ErrorCustom("Invalid Credentials", 401)
 
 
-  res.cookie("token", token, {
-    // httpOnly: process.env.NODE_ENV !== "development",
-    httpOnly: false,
-    secure: false,
-    // sameSite: "none",
+    const token = await createAccessToken({ id: userFound._id })
 
-  });
 
-  responseCustom(res, 200, {
-    id: userFound._id,
-    name: userFound.name,
-    username: userFound.username,
-    email: userFound.email,
-    photo: userFound.photo
-  })
+    res.cookie("token", token, {
+        httpOnly: process.env.NODE_ENV !== "development",
+        secure: false,
+        sameSite: "none",
+
+    });
+
+    responseCustom(res, 200, {
+        id: userFound._id,
+        name: userFound.name,
+        username: userFound.username,
+        email: userFound.email,
+        photo: userFound.photo
+    })
 
 }
 
 module.exports = {
-  register,
-  login
+    register,
+    login
 }
--- apps/frontend/src/utils/imageStatic.ts
+++ apps/frontend/src/utils/imageStatic.ts
@@ -2,8 +2,6 @@ export const imageStatic = (path: string) => {
    
     const newBaseUrl= (import.meta.env.VITE_APP_STATIC_URL as string).replace(/\/$/, '')
  
-    console.log(`${newBaseUrl}/${path}`)
-
   return `${newBaseUrl}/${path}`
 }
 
