39a95381a53f31fd687ad42e49fdad74118957f9
leonelhv <leonel.huachacav@gmail.com>
2023-08-02 23:31:57 -0500

    fix: replace close button

--- apps/frontend/src/components/Modal.tsx
+++ apps/frontend/src/components/Modal.tsx
@@ -1,21 +1,21 @@
-import IconClose from "../assets/close.svg";
-
 interface Props {
-    closeModal: () => void
-    children?: React.ReactNode
+  closeModal: () => void
+  children?: React.ReactNode
 }
 
 export default function Modal ({ closeModal, children }: Props) {
 
 
-    return (
+  return (
 
-        <div className='fixed inset-0 bg-black bg-opacity-50 z-10 grid place-content-center'>
-            <div className="bg-white w-auto m-auto py-6 px-12 rounded-lg overflow-y-auto">
-                <span onClick={closeModal}><img src={IconClose} className="w-8 hover:scale-105 transition-all" /></span>
-                {children}
-            </div>
-        </div>
+    <div className='fixed inset-0 bg-black bg-opacity-50 z-10 grid place-content-center'>
+      <div className="bg-white w-auto m-auto py-6 px-12 rounded-lg overflow-y-auto">
+        <span onClick={closeModal}>
+          <i className="fa-solid fa-xmark text-black text-2xl cursor-pointer hover:scale-105"></i>
+        </span>
+        {children}
+      </div>
+    </div>
 
-    )
+  )
 }
